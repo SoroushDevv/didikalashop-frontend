0303eff04f5b29b8836ba386d6a7cc310d979edd
soroushmoradi <soroushmoradi13.sm@gmail.com>
2025-10-29 09:56:30 -0700

    refactor some codes

--- src/Pages/Profile/Components/UserHistory/UserHistory.jsx
+++ src/Pages/Profile/Components/UserHistory/UserHistory.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React,{useState} from "react";
 import "./UserHistory.css"
 import { historyProducts } from "../../../../datas";
 
--- src/Pages/Profile/Components/UserOrderReturns/UserOrderReturn.jsx
+++ src/Pages/Profile/Components/UserOrderReturns/UserOrderReturn.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React,{ useState } from "react";
 import "./UserOrderReturn.css"
 import ErrorMessage from "../../../ErrorMessage/ErrorMessage";
 
