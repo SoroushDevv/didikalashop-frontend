5268e7b82dc2bcd3e843bbe68e52a5e812f6c37a
soroushmoradi <soroushmoradi13.sm@gmail.com>
2025-12-23 20:30:56 +0000

    solve return navigation

--- src/Pages/MobileProfile/MainPage/MobileProfile.jsx
+++ src/Pages/MobileProfile/MainPage/MobileProfile.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
-import { Outlet } from "react-router-dom";
+import { Outlet ,useNavigate} from "react-router-dom";
 import ProfileSidebar from "../../Profile/Components/ProfileSidebar/ProfileSidebar";
 import { isAuthenticated } from "../../../Utils/AuthUtils";
 import { useCurrentUser } from "../../../Hooks/useCurrentUser";
 import ErrorMessage from "../../ErrorMessage/ErrorMessage";
+import { Redo2 } from 'lucide-react';
 
 export default function MobileProfile() {
   const { currentUser, loading, error } = useCurrentUser()
@@ -32,15 +32,12 @@ export default function MobileProfile() {
       {currentUser ? (
         <div className="w-full flex flex-col gap-4">
 
-            <button
-              onclick={() => goBack()}
-              class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-            >
-              بازگشت
-            </button>
+            <div class="px-4 py-2 flex justify-start items-center  text-white rounded-lg">
+              <div className="w-8 h-8 bg-blue-300 hover:bg-blue-600 rounded-full flex justify-center items-center">
 
-   
-
+              <Redo2 className="h-5 w-5 " onClick={() => goBack()}/>
+              </div>
+            </div>
 
           <section className="w-full max-w-[1200px] py-0 px-4">
             <Outlet />
